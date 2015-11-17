e5f86d8e9fb93fe3fa2775be0abb2ce567f7cef6
ugrasergun <ugras_ergun@yahoo.com>
2015-11-17 16:44:54 +0200

    ArrayFlatter.cs was untracked.

---
+++ ArrayFlatter/ArrayFlatter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayFlatter
+{
+    public class ArrayFlatter
+    {
+        public static int[] FlatArray(object[] nestedArray)
+        {
+            if (nestedArray == null)
+                return null;
+            List<int> flatArray = new List<int>();
+            foreach (var nestedItem in nestedArray)
+            {
+                if (nestedItem is int)
+                {
+                    flatArray.Add((int)nestedItem);
+                }
+                else if (nestedItem is object[])
+                {
+                    flatArray.AddRange(FlatArray((object[])nestedItem));
+                }
+                else
+                {
+                    throw new ArgumentException("Non-integer value in array!");
+                }
+            }
+            return flatArray.ToArray();
+        }
+    }
+}
